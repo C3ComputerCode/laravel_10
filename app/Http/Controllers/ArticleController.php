28c5d9a3cce1f136b2ebe3ae9039efca49541f67
C3ComputerCode <c3computercentre@gmail.com>
2025-02-05 15:57:54 +0630

    dataSeeding

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+
+class ArticleController extends Controller
+{
+    public function index(){
+        // $data = [
+        //     [ "id" => 1, "title" => "First Article" ],
+        //     [ "id" => 2, "title" => "Second Article" ],
+        //     [ "id" => 3, "title" => "Third Article" ],
+
+        // ];
+
+        $data = Article::all();
+
+        return view("articles.index",[
+            'articles' => $data
+        ]);
+    }
+
+    public function detail($id){
+        return "Controller - Article Detail ".$id;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get("/article",function(){
+//     return "article list";
+// });
+
+Route::get('/articles',[ArticleController::class,"index"]);
+
+Route::get('/articles/detail/{id}',[ArticleController::class,"detail"]);
+
+Route::get("/articles/detail",function(){
+    return "articles detail";
+});
+
+// Route::get("/article/detail/{id}",function($id){
+//     return "article detail ".$id;
+// });
+
